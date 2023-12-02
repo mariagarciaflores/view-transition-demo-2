c65e27fc6644a689418c1ed4c66c54dbd0930da5
mariagarciaflores <mariagarciaflores>
2023-12-02 12:26:12 -0400

    Transitions applied

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { getAllCats } from "./firestore-calls/calls";
 (() => {
   const loadGallery = () => {
     getAllCats()
-      .then((cats) => document.startViewTransition(() => updateDOMCatGallery(cats)))
+      .then((cats) =>
+        document.startViewTransition(() => updateDOMCatGallery(cats))
+      )
       .catch((error) => console.error("Error", error));
   };
 
@@ -22,11 +24,30 @@ import { getAllCats } from "./firestore-calls/calls";
       catImage.src = catInfo.url;
       catImage.alt = catInfo.description;
 
+      catImage.style.viewTransitionName = `${catInfo.id}`;
+      catImage.setAttribute("data-name", `${catInfo.id}`);
+
       listItem.appendChild(catImage);
 
       catGalleryElement.appendChild(listItem);
+
+      catImage.addEventListener("click", (e) => {
+        increaseImage(e.target);
+      });
     });
   };
+
+  function increaseImage(image) {
+    const src = image.src;
+    const transitionName = image.getAttribute("data-name");
+
+    const imageViewer = document.querySelector("#cat-gallery");
+
+    document
+      .startViewTransition(() => {
+        imageViewer.innerHTML = `<img src="${src}" width="600" style="view-transition-name:${transitionName}" />`;
+      })
+  }
 
   loadGallery();
 })();
